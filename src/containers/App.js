bd9ecdb8683d29020416e58442f35c0d858914c4
guznoer <gusnurullah@gmail.com>
2021-02-14 18:09:36 +0700

    Update Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,34 +5,39 @@ import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import { connect } from 'react-redux';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = (state) => ({
-    searchfield: state.searchfield
+    searchfield: state.searchRobots.searchfield,
+    isPending: state.requestRobots.isPending,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
 });
 
 function App(props){
 
-    const [robots, setRobots] = useState([]);
-    const {searchfield, onSearchChange} = props;
+    // const [robots, setRobots] = useState([]);
+    const {searchfield, robots, onSearchChange, isPending } = props;
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {
-                setRobots(users);
-            });
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(response => response.json())
+        //     .then(users => {
+        //         setRobots(users);
+        //     });
+        props.onRequestRobots();
     }, []);
 
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    return !robots.length ? <h1>Loading...</h1> :
+    return isPending ? <h1>Loading...</h1> :
         <div className="tc">
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={onSearchChange} />
